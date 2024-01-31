b393c596b058880975d191cf8e6dae403bd4d2db
craigj915 <craigj915@gmail.com>
2024-01-31 01:50:30 +0000

    Ooops fixed globalscss import

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "./globals.scss";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
