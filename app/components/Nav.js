b20338c7d5636ff86267736b7832ad7bf4f96695
craigj915 <craigj915@gmail.com>
2024-02-06 15:57:47 +0000

    new Cv link

--- app/components/Nav.js
+++ app/components/Nav.js
@@ -35,7 +35,7 @@ const Nav = () => {
           <li>
             <a
               type="button"
-              href="https://drive.google.com/file/d/17FlsaNU30pBJK3ePNEYVXfrWfcRmiBCu/view?usp=sharing"
+              href="https://drive.google.com/file/d/17LzBzPTkhLjVt0tnQCRAW7zCEIJ0cOvH/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:text-blue-300 duration-300 drop-shadow-lg"
